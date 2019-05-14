64f2e5a67d631ce68601be16946d23eeea84faea
Fernando Mendoza R <fernandoza@gmail.com>
2019-05-14 09:56:13 -0500

    feat: populate UI and make it pretty

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -43,7 +45,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +58,16 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
+        TextView descTv = findViewById(R.id.description_tv);
+        TextView placeTv = findViewById(R.id.origin_tv);
+        TextView akaTv = findViewById(R.id.also_known_tv);
+        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+
+        descTv.setText(sandwich.getDescription());
+        placeTv.setText(sandwich.getPlaceOfOrigin());
+        akaTv.setText(TextUtils.join(", ", sandwich.getAlsoKnownAs()));
+        ingredientsTv.setText(TextUtils.join(", ", sandwich.getIngredients()));
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -3,7 +3,6 @@ package com.udacity.sandwichclub.utils;
 import com.udacity.sandwichclub.model.Sandwich;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
