1c809f3f17d6294cacc6a4a3412664e57b2b26fc
Fer <fernandoza@gmail.com>
2019-05-13 23:03:02 -0500

    feat: finish the JSON parser

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,41 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String jsonString) {
+        Sandwich sandwich = new Sandwich();
+        try {
+            JSONObject sandwichJson = new JSONObject(jsonString);
+            JSONObject name = sandwichJson.getJSONObject("name");
+            JSONArray akaJson =  name.getJSONArray("alsoKnownAs");
+            JSONArray ingredients =  sandwichJson.getJSONArray("ingredients");
+            List<String> akaList = new ArrayList<String>();
+            List<String> ingredientsList = akaList;
+
+            for(int i = 0; i < akaJson.length(); i++){
+                akaList.add(akaJson.getString(i));
+            }
+            sandwich.setAlsoKnownAs( akaList );
+            sandwich.setDescription(sandwichJson.getString("description"));
+            sandwich.setImage(sandwichJson.getString("image"));
+            for(int i = 0; i < ingredients.length(); i++){
+                ingredientsList.add(ingredients.getString(i));
+            }
+            sandwich.setIngredients( ingredientsList );
+            sandwich.setMainName(name.getString("mainName"));
+            sandwich.setPlaceOfOrigin(sandwichJson.getString("placeOfOrigin"));
+            return sandwich;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
